488c80f18badf2f7210e740183ebf9a485e3a57e
Margarita <margaritagahu@gmail.com>
2020-06-25 18:53:40 -0500

    display human time for posts and comments

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,8 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-from users.models import Profile 
+from users.models import Profile
+from datetime import datetime
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,6 +13,18 @@ class Post(models.Model):
     description = models.TextField(max_length=500, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def get_date(self):
+        time = datetime.now()
+        if self.created.day == time.day:
+            return "hace " + str(time.hour - self.created.hour) + " horas"
+        else:
+            if self.created.month == time.month:
+                return "hace " + str(time.day - self.created.day) + " días"
+            else:
+                if self.created.year == time.year:
+                    return "hace " + str(time.month - self.created.month) + " meses"
+        return self.created
 
     def __str__(self):
         return self.title
@@ -31,6 +44,18 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=200, blank=True)
     created = models.DateTimeField(auto_now=True)
+
+    def get_date(self):
+        time = datetime.now()
+        if self.created.day == time.day:
+            return str(time.hour - self.created.hour) + " h"
+        else:
+            if self.created.month == time.month:
+                return str(time.day - self.created.day) + " d"
+            else:
+                if self.created.year == time.year:
+                    return str(time.month - self.created.month) + " m"
+        return self.created
 
     def __str__(self):
         return self.text
